fbcfe7e5d283bfc29c30d1449a1f8bfb56c070d3
Michał Rudnicki <michal.rudnicki@pragmaticcoders.com>
2018-04-24 17:54:19 +0200

    Doodles

--- contracts/ProductRegistry.sol
+++ contracts/ProductRegistry.sol
@@ -12,7 +12,7 @@ Kontrakt powiniem umozliwiac pobranie tablicy ze wszystkimi zarejestrowanymi pro
 
 pragma solidity 0.4.19;
 
-import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
  * @title ProductRegistry
@@ -22,8 +22,57 @@ contract ProductRegistry is Ownable() {
 
     string public greetings = "hi";
 
+    struct Product {
+        address at;
+        uint price;
+    }
+
+    Product[] products;
+    mapping (uint => address) productAddresses;
+
     function greet() external view returns (string) {
         return greetings;
+    }
+
+    function registerProduct(address at, uint price) public onlyOwner {
+        require(price > 0);
+        uint productId = products.push(Product(at, price)) - 1;
+        productAddresses[productId] = at;
+    }
+
+    function removeProduct(address at) public onlyOwner {
+        uint productId = getProductIdByAddress(at);
+        products[productId] = Product(0, 0);
+        productAddresses[productId] = 0;
+    }
+
+    function isProductRegistered(address at) public view returns (bool) {
+        uint productId = getProductIdByAddress(at);
+        return productAddresses[productId] != 0;
+    }
+
+    function getProductIdByAddress(address at) internal view returns (uint) {
+        for (uint i = 0; i < products.length; ++ i) {
+            if (productAddresses[i] == at) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    function getProductPrice(address at) public view returns (uint) {
+        uint productId = getProductIdByAddress(at);
+        return products[productId].price;
+    }
+
+    function getAllProductAddresses() public view returns (address[]) {
+        address[] memory allAddresses;
+        for (uint i = 0; i < products.length; ++i) {
+            if (isProductRegistered(products[i].at)) {
+                allAddresses[allAddresses.length] = products[i].at; // may explode due to range error, need to run and test
+            }
+        }
+        return allAddresses;
     }
 
 }
